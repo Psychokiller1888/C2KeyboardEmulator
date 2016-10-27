638c0885f91b756c229765a62edb49c6471aa4b9
Psychokiller1888 <xXx06ksyadmin>
2016-10-27 10:11:32 +0200

    Working state :)

--- plugin/files/keyboardEmulator/runtime.js
+++ plugin/files/keyboardEmulator/runtime.js
@@ -88,9 +88,8 @@ cr.plugins_.KeyboardEmulator = function(runtime)
 	function Acts() {};
 
 	// the example action
-	Acts.prototype.doEmulate = function (keyCode)
-	{
-		jQuery.event.trigger({ type : 'keypress', which : keyCode });
+	Acts.prototype.doEmulate = function (code) {
+		jQuery.event.trigger({ type : 'keydown', which : code });
 	};
 	
 	// ... other actions here ...
@@ -102,13 +101,13 @@ cr.plugins_.KeyboardEmulator = function(runtime)
 	function Exps() {};
 	
 	// the example expression
-	Exps.prototype.MyExpression = function (ret)	// 'ret' must always be the first parameter - always return the expression's result through it!
+	/*Exps.prototype.MyExpression = function (ret)	// 'ret' must always be the first parameter - always return the expression's result through it!
 	{
 		// ret.set_int(1337);			// return our value
 		// ret.set_float(0.5);			// for returning floats
 		// ret.set_string("Hello");		// for ef_return_string
 		// ret.set_any("woo");			// for ef_return_any, accepts either a number or string
-	};
+	};*/
 	
 	// ... other expressions here ...
 	
